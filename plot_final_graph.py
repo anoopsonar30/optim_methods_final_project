139a14bc56cf583f86615ffadcb0f8d54f66b4d4
Anoop Sonar <anoopsonar30@gmail.com>
2023-12-07 18:47:51 -0500

    Computed iris trajectory durations and lengths, need to use the pickled output to solve the routing problem next

--- plot_final_graph.py
+++ plot_final_graph.py
@@ -1,12 +1,18 @@
 import pickle
-
 import ipdb
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.collections import LineCollection, PatchCollection
 
+from pydrake.solvers import MathematicalProgram, Solve, MosekSolver
+from pydrake.planning import GcsTrajectoryOptimization
+from pydrake.geometry.optimization import Point, GraphOfConvexSetsOptions, VPolytope
+# from pydrake.geometry.optimization import HPolyhedron, Iris, IrisOptions, VPolytope
+
 from floorplan import get_dests, get_floorplan_image, get_lines, get_rects, get_sources
 
+if MosekSolver().enabled():
+    print("It looks like you have a Mosek license")
 
 def main():
     image = get_floorplan_image()
@@ -51,7 +57,34 @@ def main():
     col = PatchCollection(patches, facecolor="C5", alpha=0.3, zorder=4)
     ax.add_collection(col)
 
-    fig.savefig("floorplan_final.pdf")
+    gcs = GcsTrajectoryOptimization(2)
+    
+    convex_sets = []    
+    for free_poly in free_polys:
+        convex_sets.append(VPolytope(free_poly.T))
+    
+    regions = gcs.AddRegions(regions=convex_sets, order=3)
+    source = gcs.AddRegions([Point(srcs[0, :])], order=0)
+    target = gcs.AddRegions([Point(dests[0, :])], order=0)
+    
+    gcs.AddEdges(source, regions)
+    gcs.AddEdges(regions, target)
+    
+    gcs.AddTimeCost()
+    # gcs.AddVelocityBounds(
+    #     plant.GetVelocityLowerLimits(), plant.GetVelocityUpperLimits()
+    # )
+
+    options = GraphOfConvexSetsOptions()
+    options.preprocessing = True
+    options.max_rounded_paths = 5
+    start_time = time.time()
+    traj, result = gcs.SolvePath(source, target, options)
+    print(f"GCS solved in {time.time() - start_time} seconds")
+    if not result.is_success():
+        print("Could not find a feasible path from source to destination")
+    
+    # fig.savefig("floorplan_final.pdf")
 
 
 if __name__ == "__main__":
