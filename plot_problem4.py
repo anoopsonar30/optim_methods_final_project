3ec34e14b6e64093f152d90670bd918c9b2211e9
oswinso <oswinso@gmail.com>
2023-12-08 14:45:10 -0500

    Sorted times and distances for p4

--- plot_problem4.py
+++ plot_problem4.py
@@ -4,7 +4,7 @@ import ipdb
 import matplotlib.pyplot as plt
 import numpy as np
 
-from floorplan import get_src_dst_scaled, get_wall_figure, load_gcs_trajs
+from floorplan import get_src_dst_scaled, get_wall_figure, load_gcs_trajs, load_time_and_dist
 
 
 def main():
@@ -13,6 +13,7 @@ def main():
 
     srcs, dsts = get_src_dst_scaled()
     trajs_s2d, trajs_d2d = load_gcs_trajs()
+    T_s2d, L_s2d, T_d2d, L_d2d = load_time_and_dist()
 
     n_src, n_dst = len(srcs), len(dsts)
 
@@ -20,12 +21,28 @@ def main():
     sol_dist_npz = np.load("sols/problem4_dist.npz")
 
     linestyles = ["-", "--", "-.", ":"]
+    colors = ["C2", "C4", "C5", "C6"]
 
     for npz, label in [(sol_time_npz, "time"), (sol_dist_npz, "dist")]:
+        # (n_robot, n_src, n_dst)
         x0 = npz["x0"]
+        # (n_robot, n_d2d, n_dst, n_dst)
         x1 = npz["x1"]
 
-        print("x1.shape: {}".format(x1.shape))
+        # Sort robot, first by x coordinate of start, then by x coordinates of destination.
+        tmp = 10 * np.argmax(x0.sum(axis=2), axis=1) + np.argmax(x0.sum(axis=1), axis=1)
+        robot_order = np.argsort(tmp)
+        x0 = x0[robot_order]
+        x1 = x1[robot_order]
+
+        # Compute the total time and total distance per robot.
+        total_time0 = np.sum(T_s2d * x0, axis=(1, 2))
+        total_time1 = np.sum(T_d2d * x1, axis=(1, 2, 3))
+        total_times = total_time0 + total_time1
+
+        total_dist0 = np.sum(L_s2d * x0, axis=(1, 2))
+        total_dist1 = np.sum(L_d2d * x1, axis=(1, 2, 3))
+        total_dists = total_dist0 + total_dist1
 
         ls = "-"
 
@@ -41,7 +58,7 @@ def main():
                         # ls = linestyles[rr]
                         # color = "C1"
                         # ls = "--"
-                        color = f"C{rr}"
+                        color = colors[rr]
 
                         traj = trajs_s2d[(ii, jj)]
                         ax.plot(traj[:, 0], traj[:, 1], color=color, ls=ls, alpha=0.8)
@@ -55,7 +72,7 @@ def main():
                             # ls = linestyles[rr]
                             # color = "C{}".format(kk + 2)
                             # ls = "--"
-                            color = f"C{rr}"
+                            color = colors[rr]
 
                             traj = trajs_d2d[(ii, jj)]
                             if len(traj) > 0:
@@ -64,6 +81,10 @@ def main():
                                 src, dst = dsts[ii], dsts[jj]
                                 ax.plot([src[0], dst[0]], [src[1], dst[1]], color=color, ls=ls, alpha=0.8)
 
+        total_times_str = ", ".join(["{:.2f}".format(tt) for tt in np.sort(total_times)])
+        total_dists_str = ", ".join(["{:.2f}".format(tt) for tt in np.sort(total_dists)])
+
+        ax.set_title("Time: [{}] s    Dist: [{}] m".format(total_times_str, total_dists_str))
         fig.savefig(plot_dir / f"p4_min_{label}.pdf", bbox_inches="tight")
         fig.savefig(plot_dir / f"p4_min_{label}.png", bbox_inches="tight")
         plt.close(fig)
